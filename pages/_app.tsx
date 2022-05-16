5219a0e01711285da7878c1cb3392fe5e9cb7fa4
Gustavo Restrepo <39838724+tavorestrepo98@users.noreply.github.com>
2022-05-15 22:02:38 -0500

    Primer deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import {CssBaseline, ThemeProvider} from '@mui/material';
+
+import { darkTheme } from '../themes';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
